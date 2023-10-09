32f6e1f6934d7c30a622448419fcab888e1564e2
GroovieGermanikus <groovie@mango.markets>
2023-10-09 13:21:42 +0200

    move trait

--- bin/play-with-account-fetcher-mango/src/account_fetcher_mangov4.rs
+++ bin/play-with-account-fetcher-mango/src/account_fetcher_mangov4.rs
@@ -14,22 +14,7 @@ use solana_sdk::account::{AccountSharedData, ReadableAccount};
 use solana_sdk::pubkey::Pubkey;
 
 use mango_v4::state::MangoAccountValue;
-
-#[async_trait::async_trait]
-pub trait AccountFetcher: Sync + Send {
-    async fn fetch_raw_account(&self, address: &Pubkey) -> anyhow::Result<AccountSharedData>;
-    async fn fetch_raw_account_lookup_table(
-        &self,
-        address: &Pubkey,
-    ) -> anyhow::Result<AccountSharedData> {
-        self.fetch_raw_account(address).await
-    }
-    async fn fetch_program_accounts(
-        &self,
-        program: &Pubkey,
-        discriminator: [u8; 8],
-    ) -> anyhow::Result<Vec<(Pubkey, AccountSharedData)>>;
-}
+use crate::account_fetcher_trait::AccountFetcher;
 
 // Can't be in the trait, since then it would no longer be object-safe...
 pub async fn account_fetcher_fetch_anchor_account<T: AccountDeserialize>(
